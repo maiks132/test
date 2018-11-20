7314e73debc8b3e643082bd1390784c2cd4c0761
maiks132 <maija.kolodina@gmail.com>
2018-11-20 17:27:56 +0200

    update

---
+++ Skola/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World!</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<a href="#">Links</a>
+<ul>
+	<li>1</li>
+	<li>2</li>
+	<li>3</li>
+</ul>
+
+</body>
+</html>
