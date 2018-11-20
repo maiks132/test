d694a610707d20f5a7bab64ee84bb59e40918857
maiks132 <maija.kolodina@gmail.com>
2018-11-20 17:26:53 +0200

    update

--- soclapa/resources/views/Pages/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-          <h1>{{$title}}</h1>    
-          <p>This is laravel application from the "Laravel from Scratch" Yotube series</p>     
-@endsection
