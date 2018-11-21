e5e1ed75e3cebc775f8b9b0a07086534eb6c7842
maiks132 <maija.kolodina@gmail.com>
2018-11-21 17:46:15 +0200

    update

--- Skola/boostrap/index.php
+++ Skola/boostrap/index.php
@@ -49,12 +49,13 @@
 </div>
 <br>
 <div class="container">
+    <div class="row">
 <div class="col">
   <img class="card-img-top" src="https://picsum.photos/286/180" alt="Card image cap">
-  <div class="card-body">
+ <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
+</div>
 </div>
 <div class="col">
   <img class="card-img-top" src="https://picsum.photos/286/180" alt="Card image cap">
@@ -69,6 +70,7 @@
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
+</div>
 </div>
 </div>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
