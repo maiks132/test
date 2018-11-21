d3e9b7d7a6564a3b44fa22fbca6a98b13191b12d
maiks132 <maija.kolodina@gmail.com>
2018-11-21 17:01:24 +0200

    update

---
+++ Skola/forms.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html> 
+<html> 
+<head>     
+	<meta charset="utf-8">     
+	<meta
+	name="viewport" content="windth-device-windth, initial-scale=1.0">     
+	<meta
+	http-equiv="X-UA-Compatible" content="ie=edge">     
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="css/form.css">     
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<body>
+		<div class="container"> 
+		<h1>Form</h1>  
+		<form>   
+			<div class="form-group">     <label
+					for="exampleInputEmail1">Email address</label>     
+					<input type="email" class
+					="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+					placeholder="Enter email">     
+					<small id="emailHelp" class="form-text text-
+					muted">We'll never share your email with anyone else.</small>   
+				</div>   
+				<div class="form-group">     
+					<label for="exampleInputPassword1">Password</label>
+					<input type="password" class="form-control" id="exampleInputPassword1"
+					placeholder="Password">   </div> <!--   <input type="date" name="date"
+placeholder="Select date"> -->   
+				<div class="form-group form-check"> 
+					<input
+					type="checkbox" name="remeber" class="form-check-input" id="exampleCheck1">
+					<label class="form-check-label" for="exampleCheck1">Remember me</label>
+				</div>   
+				<button type="submit" class="btn btn-primary">Submit</button> 
+			</form>
+		</div>
+</body> 
+</html>
