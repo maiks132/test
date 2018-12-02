fc455613dda1462100c6b14b8abbe5162a81561d
maiks132 <maija.kolodina@gmail.com>
2018-12-02 14:15:18 +0200

    update

---
+++ Skola/style/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Document</title>
+  </head>
+<body>
+	<div class="block">
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="custom-col1 col-sm-6 col-lg-4">
+				<p>1. One of Three Columns</p>
+			</div>
+			<div class="custom-col2 col-sm-6 col-lg-4">
+				<p>2. One of Three Columns</p>
+			</div>
+			<div class="custom-col3 col-sm-6 col-lg-4">
+				<p>3. One of Three Columns</p>
+			</div>
+		</div>
+	</div>
+
+	<script 
+ 	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+	crossorigin="anonymous">
+	</script>
+	<script 
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
+	integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" 
+	crossorigin="anonymous">
+	</script>
+	<script 
+	src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+	integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
+	crossorigin="anonymous">
+	</script>
+</body>
+</html>
+
